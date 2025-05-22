6c073b3aaab0abc3bc69c726c41044850f44084b
Ryan Kulp <ryanckulp@gmail.com>
2025-05-22 00:23:49 -0400

    Tempest refactors to work as data_provider for Weather plugin, sunrise coercion fixes

--- lib/tempest_weather_station/tempest_weather_station.rb
+++ lib/tempest_weather_station/tempest_weather_station.rb
@@ -8,6 +8,10 @@ module Plugins
     end
 
     class << self
+      def for_weather_instance(plugin_settings)
+        self.new(plugin_settings).locals
+      end
+
       def redirect_url
         query = {
           response_type: 'code',
@@ -41,36 +45,37 @@ module Plugins
 
     private
 
-    # fetching /better_forecast endpoint -> forecast.daily payload begins showing tmrw before day is over
-    # this workaround lets us retrieve today's low/high temps in case the forecast is already showing tomorrow
     def today_stats
-      @today_stats ||= begin
-        today_stats_url = "#{BASE_URL}/stats/station/#{station_id}?api_key=#{access_token}"
-        resp = HTTParty.get(today_stats_url)
-        data = JSON.parse(resp.body)
-        ts = data['stats_day'].filter { |d| d[0] == today_yyyy_mm_dd }.first
+      # TODO: get access to /stations endpoint with lat/lon bounding box to resolve evening high/low fetch
+      @today_stats ||= if plugin.keyname == 'weather'
+                         ts = daily_forecasts[0]
+                         {
+                           air_temp_low: ts['air_temp_low'],
+                           air_temp_high: ts['air_temp_high']
+                         }
+                       else
+                         # fetching /better_forecast endpoint -> forecast.daily payload begins showing tmrw before day is over
+                         # this workaround lets us retrieve today's low/high temps in case the forecast is already showing tomorrow
 
-        # based on indexing here, but +1 due to the date being prepended
-        # https://apidocs.tempestwx.com/reference/get_stats-station-station-id
-        {
-          air_temp_low: ts[4],
-          air_temp_high: ts[5]
-        }
-      end
+                         today_stats_url = "#{BASE_URL}/stats/station/#{station_id}?api_key=#{access_token}"
+                         resp = fetch(today_stats_url)
+                         data = JSON.parse(resp.body)
+                         ts = data['stats_day'].filter { |d| d[0] == today_yyyy_mm_dd }.first
+
+                         # based on indexing here, but +1 due to the date being prepended
+                         # https://apidocs.tempestwx.com/reference/get_stats-station-station-id
+                         {
+                           air_temp_low: ts[4],
+                           air_temp_high: ts[5]
+                         }
+                       end
     end
 
     # rubocop:disable Metrics/AbcSize
     def forecast
       @forecast ||= begin
-        url = "#{BASE_URL}/better_forecast?station_id=#{station_id}&units_wind=#{units_wind}&units_precip=#{units_precip}&token=#{access_token}"
-        resp = HTTParty.get(url)
-        data = JSON.parse(resp.body)
-
-        right_now = data['current_conditions']
-        daily_forecasts = data['forecast']['daily']
-
-        today_forecast_idx = daily_forecasts.index { |d| d['day_num'] == today_day_number }
-        tmrw_forecast_idx = daily_forecasts.index { |d| d['day_num'] == tomorrow_day_number }
+        today_forecast_idx = daily_forecasts.index { |d| Time.at(d['day_start_local']).in_time_zone(timezone).to_date.to_s == today_yyyy_mm_dd }
+        tmrw_forecast_idx = daily_forecasts.index { |d| Time.at(d['day_start_local']).in_time_zone(timezone).to_date.to_s == tomorrow_yyyy_mm_dd }
 
         today = today_forecast_idx ? daily_forecasts[today_forecast_idx] : today_stats
         tomorrow = daily_forecasts[tmrw_forecast_idx]
@@ -81,10 +86,10 @@ module Plugins
             humidity: right_now['relative_humidity'],
             icon: right_now['icon'],
             temperature: smart_round_in_desired_unit(right_now['air_temperature']),
-            sunrise: today['sunrise'] ? Time.at(today['sunrise']).in_time_zone(user.tz).strftime("%H:%m") : '',
-            sunset: today['sunset'] ? Time.at(today['sunset']).in_time_zone(user.tz).strftime("%H:%m") : '',
-            sunrise_unix: Time.at(today['sunrise']).to_i,
-            sunset_unix: Time.at(today['sunset']).to_i,
+            sunrise: today['sunrise'] ? Time.at(today['sunrise']).in_time_zone(user.tz).strftime("%H:%M") : '',
+            sunset: today['sunset'] ? Time.at(today['sunset']).in_time_zone(user.tz).strftime("%H:%M") : '',
+            sunrise_unix: today['sunrise'] ? Time.at(today['sunrise']).to_i : '',
+            sunset_unix: today['sunset'] ? Time.at(today['sunset']).to_i : '',
             wind: { direction_cardinal: right_now['wind_direction_cardinal'], gust: right_now['wind_gust'], units: units_wind }
           },
           today: {
@@ -102,13 +107,29 @@ module Plugins
             maxtemp: smart_round_in_desired_unit(tomorrow['air_temp_high']),
             day_override: forecast_day_override('tomorrow'),
             conditions: tomorrow['conditions'],
-            uv_index: data['forecast']['hourly'][12]['uv']&.to_i, # unlike right_now['uv'] this val a float; retrieves mid-day (12:00) tmrw, not avail in 'tomorrow' block
+            uv_index: hourly_forecasts[12]['uv']&.to_i, # unlike right_now['uv'] this val a float; retrieves mid-day (12:00) tmrw, not avail in 'tomorrow' block
             precip: { icon: tomorrow['precip_icon'], probability: tomorrow['precip_probability'] }
           }
         }
       end
     end
     # rubocop:enable Metrics/AbcSize
+
+    def forecast_data
+      @forecast_data ||= begin
+        resp = HTTParty.get(forecast_url)
+        JSON.parse(resp.body)
+      end
+    end
+
+    def forecast_url
+      @forecast_url ||= if plugin.keyname == 'weather'
+                          lat, lon = settings['lat_lon'].split(',')
+                          "#{BASE_URL}/better_forecast?lat=#{lat}&lon=#{lon}&units_precip=#{units_precip}&snap_to_nearest_owned_station=true&api_key=#{Rails.application.credentials.plugins.weather.tempest_api_key}"
+                        else
+                          "#{BASE_URL}/better_forecast?station_id=#{station_id}&units_wind=#{units_wind}&units_precip=#{units_precip}&token=#{access_token}"
+                        end
+    end
 
     def station_id
       @station_id ||= begin
@@ -128,8 +149,6 @@ module Plugins
     def forecast_day_override(day)
       return nil unless forecast_headings == 'absolute_date'
 
-      today = DateTime.now.in_time_zone(user.tz || 'America/New_York')
-
       case day
       when 'today'
         today.strftime("%b %d")
@@ -138,13 +157,34 @@ module Plugins
       end
     end
 
-    def conditions
-      forecast[:today][:conditions]
+    # override Tempest defaults with native Weather icons on a case by case basis based on user feedback
+    def icon_selector(icon)
+      native_icon_base_uri = "#{Rails.application.credentials.base_url}/images/plugins/weather"
+
+      case icon
+      when 'clear-day'
+        "#{native_icon_base_uri}/wi-day-sunny.svg"
+      when 'clear-night'
+        "#{native_icon_base_uri}/wi-night-clear.svg"
+      else
+        "https://tempestwx.com/images/Updated/#{icon}.svg"
+      end
     end
 
     # only show graphic for today, not future days
     def weather_image
-      forecast[:right_now][:icon]
+      icon = forecast[:right_now][:icon]
+      icon_selector(icon)
+    end
+
+    def today_weather_image
+      icon = forecast[:today][:icon]
+      icon_selector(icon)
+    end
+
+    def tomorrow_weather_image
+      icon = forecast[:tomorrow][:icon]
+      icon_selector(icon)
     end
 
     def feels_like
@@ -159,14 +199,6 @@ module Plugins
       forecast[:right_now][:humidity]
     end
 
-    def today_weather_image
-      forecast[:today][:icon]
-    end
-
-    def tomorrow_weather_image
-      forecast[:tomorrow][:icon]
-    end
-
     def smart_round_in_desired_unit(temp)
       return temp if units == 'c'
 
@@ -177,6 +209,14 @@ module Plugins
       (temp * 9 / 5) + 32
     end
 
+    def right_now = forecast_data['current_conditions']
+
+    def hourly_forecasts = forecast_data['forecast']['hourly']
+
+    def daily_forecasts = forecast_data['forecast']['daily']
+
+    def conditions = forecast[:today][:conditions]
+
     def forecast_headings = settings['forecast_headings']
 
     def units = settings['units'].downcase == 'metric' ? 'c' : 'f'
@@ -185,11 +225,11 @@ module Plugins
 
     def units_precip = settings['units_precip']
 
-    def today_yyyy_mm_dd = DateTime.now.in_time_zone(user.tz || 'America/New_York').strftime("%Y-%m-%d")
+    def today = user.datetime_now
 
-    def today_day_number = today_yyyy_mm_dd.to_date.mday
+    def today_yyyy_mm_dd = today.strftime("%Y-%m-%d")
 
-    def tomorrow_day_number = (today_yyyy_mm_dd.to_date + 1.days).mday
+    def tomorrow_yyyy_mm_dd = (today + 1.day).strftime("%Y-%m-%d")
 
     # IDEA: allow multiple devices; easy to fetch + grab all data, not easy to lay out however
     def device_id = settings['tempest_weather_station_devices'].to_s
