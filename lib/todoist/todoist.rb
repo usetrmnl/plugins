43f6f00572ee99a4de3ba522948418fa85093a7f
Ryan Kulp <dm@ryanckulp.com>
2025-06-09 21:53:07 -0400

    refactor Todoist into V2

--- lib/todoist/todoist.rb
+++ lib/todoist/todoist.rb
@@ -1,82 +1,27 @@
 module Plugins
-  class Todoist < Base
+  class TodoistV2 < Base
     TASKS_URL = 'https://api.todoist.com/rest/v2/tasks'.freeze
 
     # Extracted from Todoist Web App API Calls to `https://app.todoist.com/api/v9.223/sync`
     FILTER_DATE_OPTIONS_MAP = {
-      all:          nil,
-      today:        'overdue | today',
-      this_week:    'due before: next week',
-      next_7_days:  'overdue | next 7 days',
-      this_month:   'due before: first day',
+      all: nil,
+      today: 'overdue | today',
+      this_week: 'due before: next week',
+      next_7_days: 'overdue | next 7 days',
+      this_month: 'due before: first day',
       next_30_days: 'overdue | next 30 days',
-      no_date:      'no date'
-    }
+      no_date: 'no date'
+    }.with_indifferent_access
 
     FILTER_DEADLINE_OPTIONS_MAP = {
-      all:          nil,
-      today:        'deadline before: tomorrow',
-      this_week:    'deadline before: next week',
-      next_7_days:  'deadline before: in 7 days',
-      this_month:   'deadline before: first day',
+      all: nil,
+      today: 'deadline before: tomorrow',
+      this_week: 'deadline before: next week',
+      next_7_days: 'deadline before: in 7 days',
+      this_month: 'deadline before: first day',
       next_30_days: 'deadline before: in 30 days',
-      no_deadline:  'no deadline'
-    }
-
-    TaskDue = Struct.new(
-      :date,
-      :is_recurring,
-      :datetime,
-      :string,
-      :timezone,
-      keyword_init: true
-    ) do
-      def initialize(*args)
-        super
-        self.date     = Date.parse(date) if date
-        self.datetime = DateTime.parse(datetime) if datetime
-      end
-    end
-
-    TaskDeadline = Struct.new(
-      :date,
-      keyword_init: true
-    ) do
-      def initialize(*args)
-        super
-        self.date = Date.parse(date) if date
-      end
-    end
-
-    Task = Struct.new(
-      :creator_id,
-      :created_at,
-      :assignee_id,
-      :assigner_id,
-      :comment_count,
-      :is_completed,
-      :content,
-      :description,
-      :due,
-      :deadline,
-      :duration,
-      :id,
-      :labels,
-      :order,
-      :priority,
-      :project_id,
-      :section_id,
-      :parent_id,
-      :url,
-      keyword_init: true
-    ) do
-      def initialize(*args)
-        super
-        self.due        = TaskDue.new(due) if due
-        self.deadline   = TaskDeadline.new(deadline) if deadline
-        self.created_at = DateTime.parse(created_at) if created_at
-      end
-    end
+      no_deadline: 'no deadline'
+    }.with_indifferent_access
 
     def locals
       { tasks: }
@@ -86,9 +31,9 @@ module Plugins
       def redirect_url
         query = {
           response_type: 'code',
-          client_id: Rails.application.credentials.plugins[:todoist][:client_id],
+          client_id: Rails.application.credentials.plugins[:todoist_v2][:client_id],
           scope: 'data:read',
-          redirect_uri: "#{Rails.application.credentials.base_url}/plugin_settings/todoist/redirect"
+          redirect_uri: "#{Rails.application.credentials.base_url}/plugin_settings/todoist_v2/redirect"
         }.to_query
         "https://todoist.com/oauth/authorize?#{query}"
       end
@@ -96,9 +41,9 @@ module Plugins
       def fetch_access_token(code)
         body = {
           grant_type: "authorization_code",
-          client_id: Rails.application.credentials.plugins[:todoist][:client_id],
-          client_secret: Rails.application.credentials.plugins[:todoist][:client_secret],
-          redirect_uri: "#{Rails.application.credentials.base_url}/plugin_settings/todoist/redirect",
+          client_id: Rails.application.credentials.plugins[:todoist_v2][:client_id],
+          client_secret: Rails.application.credentials.plugins[:todoist_v2][:client_secret],
+          redirect_uri: "#{Rails.application.credentials.base_url}/plugin_settings/todoist_v2/redirect",
           code: code
         }
         response = HTTParty.post("https://todoist.com/oauth/access_token", body:)
@@ -108,7 +53,7 @@ module Plugins
       # @see https://developer.todoist.com/rest/v2/#get-all-projects
       def projects(access_token)
         response = HTTParty.get('https://api.todoist.com/rest/v2/projects', headers: headers(access_token))
-        response.map { |m| { m['name'] => m['id'] } }&.push({ 'Today' => 'today' })
+        response.parsed_response&.map { |m| { m['name'] => m['id'] } }&.push({ 'Today' => 'today' })
       end
 
       # @see https://developer.todoist.com/rest/v2/#get-all-personal-labels
@@ -122,12 +67,14 @@ module Plugins
 
     private
 
-    def access_token = settings.dig('todoist', 'access_token')
+    def access_token
+      settings.dig('todoist_v2', 'access_token')
+    end
 
     def tasks
       response = fetch(TASKS_URL, query:, headers: Plugins::Todoist.headers(access_token))
+      tasks = response.parsed_response
 
-      tasks = response.map { Task.new(it) }
       tasks = tasks_sort(tasks)
       tasks = tasks_group(tasks)
 
@@ -135,75 +82,79 @@ module Plugins
     end
 
     def query
-      if today_view?
-        {
-          filter: tasks_filter_today
-        }
-      else
-        {
-          project_id: settings['todoist__project_id'],
-          filter:     tasks_filter_project
-        }
-      end
+      qry = if today_view?
+              {
+                filter: filter_today
+              }
+            else
+              {
+                project_id: settings['todoist_v2_project_id'],
+                filter: tasks_filter_project
+              }
+            end
+
+      qry.delete(:filter) if qry[:filter].blank? # if '' or nil, API response 'Invalid argument value'
+      qry
     end
 
-    def today_view? = settings['todoist__project_id'] == 'today'
+    def today_view? = settings['todoist_v2_project_id'] == 'today'
     def today = user.datetime_now.end_of_day
 
     # @note The internal Todoist API wraps the "Today" view, so this is a best guess
     #   to match the behavior of the Todoist Web App.
-    def tasks_filter_today
-      tasks_filter_compile([
-        FILTER_DATE_OPTIONS_MAP.fetch(:today),
-        FILTER_DEADLINE_OPTIONS_MAP.fetch(:today)
-      ], joiner: '|')
+    def filter_today
+      filter_compile([
+                       FILTER_DATE_OPTIONS_MAP.fetch(:today),
+                       FILTER_DEADLINE_OPTIONS_MAP.fetch(:today)
+                     ], joiner: '|')
     end
 
     # @return [String] `(@label) & (p1 | p2 | p3) & (due before: next week) & (deadline before: tomorrow)`
     def tasks_filter_project
       # Order based on observed Todoist Web App API Calls
-      tasks_filter_compile([
-        filter_labels,
-        filter_priority,
-        filter_date,
-        filter_deadline
-      ])
+      filter_compile([
+                       filter_labels,
+                       filter_priority,
+                       filter_date,
+                       filter_deadline
+                     ])
     end
 
     # @return [String] `today` or `overdue | today`
-    def tasks_filter_date
-      FILTER_DATE_OPTIONS_MAP.fetch(settings['todoist__filter_date'])
+    def filter_date
+      FILTER_DATE_OPTIONS_MAP.fetch(settings['filter_date'])
     end
 
     # @return [String] `deadline before: tomorrow`
-    def tasks_filter_deadline
-      FILTER_DEADLINE_OPTIONS_MAP.fetch(settings['todoist__filter__deadline'])
+    def filter_deadline
+      FILTER_DEADLINE_OPTIONS_MAP.fetch(settings['filter_deadline'])
     end
 
     # @return [String] `p1` or `(p1 | p2)`
-    def tasks_filter_priority
-      settings['todoist__filter__priority']
+    def filter_priority
+      settings['filter_priority']
     end
 
     # @return [String] `@thing` or `(@thing_one | @thing_two)`
-    def tasks_filter_labels
-      return if settings['todoist__filter__labels'].blank?
+    def filter_labels
+      return if settings['filter_labels'].blank?
 
-      settings['todoist__filter__labels'].map { "@#{_1}" }
+      settings['filter_labels'].map { "@#{it}" }
     end
 
-    def tasks_filter_compile(filter_groups, joiner: '&')
+    def filter_compile(filter_groups, joiner: '&')
       filter_groups.compact.map do |filter_group|
-        tasks_filter_group_join(filter_group)
+        filter_group_join(filter_group)
       end.join(" #{joiner} ")
     end
 
-    def tasks_filter_group_join(filter_group)
+    def filter_group_join(filter_group)
+      filter_group = [filter_group] if filter_group.is_a?(String)
       "(#{filter_group.join(' | ')})"
     end
 
     def tasks_sort(tasks)
-      case settings['todoist__sort__sorting']
+      case settings['sort_sorting']
       when 'manual'
         sort_by_manual(tasks)
       when 'name'
@@ -221,46 +172,46 @@ module Plugins
       end
     end
 
-    def tasks_sort_by_manual(tasks)
-      tasks.sort_by(&:order)
+    def sort_by_manual(tasks)
+      tasks.sort_by { it['order'] }
     end
 
-    def tasks_sort_by_name(tasks)
+    def sort_by_name(tasks)
       tasks.sort_by do |task|
-        task.content.downcase
+        task['content'].downcase
       end
     end
 
-    def tasks_sort_by_date(tasks)
+    def sort_by_date(tasks)
       tasks.sort_by do |task|
-        task.due&.date || Date::Infinity.new
+        task.dig('due', 'date') ? Date.parse(task.dig('due', 'date')) : Date::Infinity.new
       end
     end
 
-    def tasks_sort_by_date_added(tasks)
-      tasks.sort_by(&:created_at)
+    def sort_by_date_added(tasks)
+      tasks.sort_by { it['created_at'] }
     end
 
-    def tasks_sort_by_deadline(tasks)
+    def sort_by_deadline(tasks)
       tasks.sort_by do |task|
-        task.deadline&.date || Date::Infinity.new
+        task.dig('deadline', 'date') ? Date.parse(task.dig('deadline', 'date')) : Date::Infinity.new
       end
     end
 
-    def tasks_sort_by_priority(tasks)
-      tasks.sort_by(&:priority)
+    def sort_by_priority(tasks)
+      tasks.sort_by { it['priority'] }
     end
 
     def tasks_group(tasks)
-      case settings['todoist__group__grouping']
+      case settings['group_grouping']
       when 'date'
-        tasks_group_by_date(tasks)
+        group_by_date(tasks)
       when 'date_added'
-        tasks_group_by_date_added(tasks)
+        group_by_date_added(tasks)
       when 'deadline'
-        tasks_group_by_deadline(tasks)
+        group_by_deadline(tasks)
       when 'priority'
-        tasks_group_by_priority(tasks)
+        group_by_priority(tasks)
       when 'labels'
         tasks_group_by_labels(tasks)
       else
@@ -272,17 +223,17 @@ module Plugins
     # May 25 ‧ Today ‧ Sunday
     # Jun 6 ‧ Friday
     # No date
-    def tasks_group_by_date(tasks)
+    def group_by_date(tasks)
       tasks.group_by do |task|
-        tasks_group_format_date_title(task.due&.date, none: 'No date')
+        tasks_group_format_date_title(task.dig('due', 'date'), none: 'No date')
       end
     end
 
     # May 25 ‧ Today ‧ Sunday
     # Jun 6 ‧ Friday
-    def tasks_group_by_date_added(tasks)
-      groups = tasks.group_by do |task|
-        tasks_group_format_date(task.created_at)
+    def group_by_date_added(tasks)
+      tasks.group_by do |task|
+        tasks_group_format_date(task['created_at'])
       end
     end
 
@@ -290,9 +241,9 @@ module Plugins
     # May 25 ‧ Today ‧ Sunday
     # Jun 6 ‧ Friday
     # No deadline
-    def tasks_group_by_deadline(tasks)
+    def group_by_deadline(tasks)
       tasks.group_by do |task|
-        tasks_group_format_date_title(task.deadline&.date, none: 'No deadline')
+        tasks_group_format_date_title(task.dig('deadline', 'date'), none: 'No deadline')
       end
     end
 
@@ -300,9 +251,9 @@ module Plugins
     # Priority 2
     # Priority 3
     # Priority 4
-    def tasks_group_by_priority(tasks)
+    def group_by_priority(tasks)
       tasks.group_by do |task|
-        "Priority #{task.priority}"
+        "Priority #{task['priority']}"
       end
     end
 
@@ -312,11 +263,11 @@ module Plugins
       groups = {}
 
       tasks.each do |task|
-        if task.labels.blank?
+        if task['labels'].empty?
           groups['No label'] ||= []
           groups['No label'] << task
         else
-          task.labels&.each do |label|
+          task['labels']&.each do |label|
             groups[label] ||= []
             groups[label] << task
           end
@@ -348,7 +299,6 @@ module Plugins
       segments.join(' ‧ ')
     end
 
-
     def tasks_format(data)
       case data
       when Array
@@ -358,18 +308,18 @@ module Plugins
           tasks.map { tasks_extract(it) }
         end
       else
-        fail "Invalid tasks format: #{tasks.class}"
+        raise "Invalid tasks format: #{tasks.class}"
       end
     end
 
     def tasks_extract(task)
       {
-        is_completed:     task.is_completed,
-        content:          task.content,
-        description:      task.description,
-        due_date:         task.due&.date,
-        due_is_recurring: task.due&.is_recurring,
-        dateline_date:    task.deadline&.date,
+        is_completed: task['is_completed'],
+        content: task['content'],
+        description: task['description'],
+        due_date: task.dig('due', 'date'),
+        due_is_recurring: task.dig('due', 'is_recurring'),
+        dateline_date: task.dig('deadline', 'date')
       }
     end
   end
